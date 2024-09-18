4764412677eb1e132b7de4ccbfa59106f646fa9a
saumittras <saumittra.eee@gmail.com>
2024-09-18 21:07:54 +0600

    added login validation and added a home page after loged in

--- js/login.js
+++ js/login.js
@@ -5,14 +5,34 @@
 // document.getElementById().addEventListiner('click',fun)
 
 // step-1 set event handler
+// document
+//   .getElementById("button-login")
+//   .addEventListener("click", function (event) {
+//     //step-2: prevent default behavior (prevent reloading browser)
+//     event.preventDefault(); //  vajal to begainers
+
+//     // step-3: get the phone number and pin number
+//     const phoneNumber = document.getElementById("phone-number").value;
+//     const pinNumber = document.getElementById("pin-number").value;
+//     console.log(phoneNumber, pinNumber);
+//     // step 4: validate phone and pin
+//     //this is temporary. You should do like this
+//     if (phoneNumber === "6" && pinNumber === "1234") {
+//       console.log("you are logged in");
+//     } else {
+//       alert("wrong passward");
+//     }
+//   });
 document
   .getElementById("button-login")
   .addEventListener("click", function (event) {
-    //step-2: prevent default behavior (prevent reloading browser)
-    event.preventDefault(); //  vajal to begainers
-    console.log("login Button Clicked");
-
-    // step-3: get the phone number
+    event.preventDefault();
     const phoneNumber = document.getElementById("phone-number").value;
-    console.log(phoneNumber);
+    const pinNumber = document.getElementById("pin-number").value;
+    if (phoneNumber === "6" && pinNumber === "1234") {
+      console.log("You are loged in");
+      window.location.href = "/home.html";
+    } else {
+      alert(" wrong Mobile and Pin Number");
+    }
   });
