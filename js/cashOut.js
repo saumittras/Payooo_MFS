cd42ea021d6f39d1b456a085222c62d0832d5a22
saumittras <saumittra.eee@gmail.com>
2024-09-19 11:48:47 +0600

    added cash out handelar

---
+++ js/cashOut.js
@@ -0,0 +1,18 @@
+document
+  .getElementById("btn-cash-out")
+  .addEventListener("click", function (event) {
+    // prevent default cash out event
+    event.preventDefault();
+
+    const cashOutAmount = document.getElementById("input-cash-out").value;
+    const cashOutPinNumber =
+      document.getElementById("input-cash-out-pin").value;
+    const currentBlance = document.getElementById("total-amount").innerText;
+    console.log(currentBlance);
+    console.log(cashOutAmount, cashOutPinNumber);
+    if (cashOutPinNumber === "1234") {
+      const newBlance = parseInt(currentBlance) - cashOutAmount;
+      console.log(newBlance);
+      document.getElementById("total-amount").innerText = newBlance;
+    }
+  });
